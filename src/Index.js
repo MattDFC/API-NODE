42b592b3cb84ff4564600ebde6f5480d9634d04f
PelaFaCov <135472456+PelaFaCov@users.noreply.github.com>
2025-07-23 00:27:51 -0300

    Final

--- src/Index.js
+++ src/Index.js
@@ -12,7 +12,7 @@ import productsRouter from "./routes/productosRoutes.js";
 app.use("/api", productsRouter);
 
 import authRouter from "./routes/authRoutes.js";
-app.use(authRouter);
+app.use("/api/auth", authRouter);
 
 app.use((req, res, next) => {
     res.status(404).json({ error: "Not found"});
