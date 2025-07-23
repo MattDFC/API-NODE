0e7ebd0e73386bedde8fa93305db5f45c1998b8b
PelaFaCov <135472456+PelaFaCov@users.noreply.github.com>
2025-07-22 23:57:46 -0300

    Final

--- src/controllers/authcontrollers.js
+++ src/controllers/authcontrollers.js
@@ -1,27 +1,28 @@
 import jwt from "jsonwebtoken";
+import { manejarError } from "../services/manejoErrores.js";
 
 const default_user = {
     id: 1,
-    email: "user@email.com",
-    password: "strongPass123",
+    email: "JeanPaulCrack@email.com",
+    password: "mejorDocente2025",
 };
 
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-
         const user = { id: 1, email };
-
         if (email === default_user.email && password === default_user.password) {
             const payload = { id: user.id };
-            const expiration = { expiresIn: "1h" };
+            const expiration = { expiresIn: "1y" };
             const token = jwt.sign(payload, process.env.JWT_SECRET, expiration);
-            return res.json({ token });
+            const nombreUsuario = email.split("@")[0];
+            return res.json({
+                message: `LOGIN OK! BIENVENIDO! ${nombreUsuario} - su token es: ${token}`
+            });
+        } else {
+            return res.status(401).json({ error: "Sus credenciales no coinciden." });
         }
-
-        return res.status(401).json({ error: "Sus credenciales no coinciden." });
-
     } catch (err) {
-        manejarError(res, err, "Error en login"); // si estás usando tu helper
+        manejarError(res, err, "Error en el login");
     }
 };
